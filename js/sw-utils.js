c3842a430bfb7cca8a6aaec3c12ec3cc6cb76ff9
Orlin Alvarado <oalvaradomorales@gmail.com>
2023-11-23 17:35:55 -0600

    Primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,12 @@
+// Guardar en el cache dinamico
+
+function actualizaCacheDinamico(dynamiCache, req, res) {
+  if ( res.ok ) {
+    return caches.open( dynamiCache ).then( cache => {
+      cache.put( req, res.clone());
+      return res.clone();
+    })
+  } else {
+    return res;
+  }
+}
